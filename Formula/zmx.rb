ce3320d06499747c5129d764567d6fa14ea8d2de
Eric Bower <me@erock.io>
2026-02-20 20:40:06 -0500

    publish v0.4.0

--- Formula/zmx.rb
+++ Formula/zmx.rb
@@ -1,28 +1,28 @@
 class Zmx < Formula
   desc "Session persistence for terminal processes"
   homepage "https://github.com/neurosnap/zmx"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://zmx.sh/a/zmx-0.3.0-macos-aarch64.tar.gz"
-      sha256 "ca3819bdbe3b340fd71bebbb505a5293d823cce22a9986b4a880a12d15fd9bf9"
+      url "https://zmx.sh/a/zmx-0.4.0-macos-aarch64.tar.gz"
+      sha256 "5b9bfd7358e904f1b5238421ddf5a87ab1445f483cb8e29e3d46519922c4bd8a"
     end
     on_intel do
-      url "https://zmx.sh/a/zmx-0.3.0-macos-x86_64.tar.gz"
-      sha256 "ca9627bafe1c3786d3d7436ab58b56c76954f3e1a0c3ca516b5afadd0e659836"
+      url "https://zmx.sh/a/zmx-0.4.0-macos-x86_64.tar.gz"
+      sha256 "b8f7abf36e042ba61f1c5148f7cba27da811eea488b55ca4eeb88dae247beb52"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://zmx.sh/a/zmx-0.3.0-linux-aarch64.tar.gz"
-      sha256 "393316cc6cce8cf64676be218f74d336a6c6d8e71173421f77440b680a1194b4"
+      url "https://zmx.sh/a/zmx-0.4.0-linux-aarch64.tar.gz"
+      sha256 "c4d287e6fed0f1f3cfdbc192712269fded52339984a8556461aabdd6b9754ed7"
     end
     on_intel do
-      url "https://zmx.sh/a/zmx-0.3.0-linux-x86_64.tar.gz"
-      sha256 "fcaff1581eb5a6a3e59781aad77a8ca069b4435bc2fec413dd323b45a4dfdf32"
+      url "https://zmx.sh/a/zmx-0.4.0-linux-x86_64.tar.gz"
+      sha256 "fae6d226b98f8dfeea0976ff2f9ef1632cf0e9fda4cb921d08f2ae9ebd95a378"
     end
   end
 
