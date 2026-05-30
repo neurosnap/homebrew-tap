56c15b3854238bdf7cd61b352aafb7fe699b84ad
Eric Bower <me@erock.io>
2026-05-30 09:27:55 -0400

    chore: update shasum

--- Formula/zmx.rb
+++ Formula/zmx.rb
@@ -7,22 +7,22 @@ class Zmx < Formula
   on_macos do
     on_arm do
       url "https://zmx.sh/a/zmx-0.6.0-macos-aarch64.tar.gz"
-      sha256 "621b85f25a1c73399e4ee46f482afc7cffb4638446e8d0eef5acaa57c2b79b4e"
+      sha256 "7f1e4d967d41dea0df76bc7c5dd0d5795e7e54fd657a5f0c74fbfb2c0699390e"
     end
     on_intel do
       url "https://zmx.sh/a/zmx-0.6.0-macos-x86_64.tar.gz"
-      sha256 "bcc9097a592b0e1cce1f43979341a5f7d0eb7ca39de60078770737761023e046"
+      sha256 "576cf81cd7df5354a49f328d0f3c571d5cf601e257eb6fb794890c79c7fb713e"
     end
   end
 
   on_linux do
     on_arm do
       url "https://zmx.sh/a/zmx-0.6.0-linux-aarch64.tar.gz"
-      sha256 "da55aee752d54be60d36d9f4f9521bccb3770410c2b1771bec846d5a24b49bfe"
+      sha256 "dbbb3df7428bbe645cc26f34dd67b61b07953a8ce3daa6f3e501c49d392724f8"
     end
     on_intel do
       url "https://zmx.sh/a/zmx-0.6.0-linux-x86_64.tar.gz"
-      sha256 "46e2b458f3247c117bc39e4eb959b58c4e5ec23fc62d776411e3dcb431bd2e3d"
+      sha256 "7ee4b12150dd0d736d271ba1cb06942244c10b857841a663517297ac65c720dd"
     end
   end
 
