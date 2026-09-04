f6d831d1faebd33b09771054cc20e0979d18bbc9
Eric Bower <me@erock.io>
2026-09-03 20:44:24 -0400

    chore: v0.8.0

--- Formula/zmx.rb
+++ Formula/zmx.rb
@@ -1,28 +1,28 @@
 class Zmx < Formula
   desc "Session attach/detach for the terminal"
   homepage "https://github.com/neurosnap/zmx"
-  version "0.7.1"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://zmx.sh/a/zmx-0.7.1-macos-aarch64.tar.gz"
-      sha256 "86ce4c0eeb6b8448d058390fabe4a5aff323c4fc1e64ed578d48ab75a0f59a79"
+      url "https://zmx.sh/a/zmx-0.8.0-macos-aarch64.tar.gz"
+      sha256 "16efdbb1269b2ba2e8910797c5def01e20e30161a11831d8ec649b7d239cf32f"
     end
     on_intel do
-      url "https://zmx.sh/a/zmx-0.7.1-macos-x86_64.tar.gz"
-      sha256 "ea4eb40b6d5b6b42525f26447254964e68f74766f6825841181b805cb944ab53"
+      url "https://zmx.sh/a/zmx-0.8.0-macos-x86_64.tar.gz"
+      sha256 "7b0f27472cad59de1059c0c6e346905714f49fdb40b35f31ec498d58bbd4ad8a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://zmx.sh/a/zmx-0.7.1-linux-aarch64.tar.gz"
-      sha256 "899f9dc743ef72dcdeab3072667c33f507247a960e5c4b7482da54fff7fdae09"
+      url "https://zmx.sh/a/zmx-0.8.0-linux-aarch64.tar.gz"
+      sha256 "09978ee2ee0813ed1f2979798b33da69b410c6e356888345e04f957f7487fd13"
     end
     on_intel do
-      url "https://zmx.sh/a/zmx-0.7.1-linux-x86_64.tar.gz"
-      sha256 "ec82d753e12537b79a76bce73399d57698e529f4744eb5a1a9bcfa6fda7c4b25"
+      url "https://zmx.sh/a/zmx-0.8.0-linux-x86_64.tar.gz"
+      sha256 "cd928e56fc5d2f1d71b1aa5d0e2ea39c96d4aea77cd047260932874c98eac5cb"
     end
   end
 
